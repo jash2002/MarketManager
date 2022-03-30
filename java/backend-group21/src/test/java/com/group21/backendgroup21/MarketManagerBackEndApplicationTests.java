2747d9fa8449734a159aaead40de1339c8accf08
Jonathan Ash <ashjonathan2002@gmail.com>
2022-03-30 13:59:09 +0100

    added spring

---
+++ java/backend-group21/src/test/java/com/group21/backendgroup21/MarketManagerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group21.backendgroup21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketManagerBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

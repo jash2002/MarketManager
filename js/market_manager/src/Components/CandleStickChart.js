19960929518d9c8a3c1fc670624371d507999daf
jamesherbert24 <james.herbert2001@gmail.com>
2022-04-12 15:19:11 +0100

    Created CandlestickChart component
    Installed required package - react-apexcharts.
    Started basis of CandleStickChart component to be used within card on dashboard.
    Added component to home page for testing.

---
+++ js/market_manager/src/Components/CandleStickChart.js
@@ -0,0 +1,107 @@
+import React from "react";
+import Chart from "react-apexcharts";
+/**
+ * Component to query API and produce Candlestick Chart.
+ * @component
+ * @extends React.Component
+ */
+class CandleStickChart extends React.Component {
+    /**
+     * Set up new blank Candlestick chart.
+     */
+    constructor(props) {
+        super(props);
+        this.fetchStock = this.fetchStock.bind(this);
+        this.state = {
+            options: {
+                chart: {
+                    type: 'candlestick',
+                    height: 350
+                  },
+                title: {
+                    text: props.ticker,
+                    align: 'left'
+                },
+                xaxis: {
+                    type: 'datetime'
+                },
+                yaxis: {
+                    tooltip: {
+                      enabled: true
+                    }
+                },
+                noData: {
+                    text: "loading..."
+                }
+            },
+            series: [],
+            type: props.type,
+            ticker: props.ticker,
+            interval: props.interval,
+            outputCount: props.outputCount,
+            height: props.height,
+            width: props.width
+        };
+    }
+
+    /**
+     * Fetch stock data to API and store in state.
+     */
+    fetchStock() {
+        /**
+         * TODO: Fetch API key from backend.
+         * Unsafe to really store in React files, and have it on github.
+         * For now, copy from discord, and remove before commiting.
+         */
+        const API_KEY = ''
+        let API_Call = 'https://api.twelvedata.com/'+this.state.type+'?symbol='+this.state.ticker+'&interval='+this.state.interval+'&output='+this.state.outputCount+'&apikey='+API_KEY;
+        const that = this;
+        fetch(API_Call)
+        .then(
+            function(response) {
+                return response.json();
+            }
+        )
+        .then(
+            function(data) {
+                var parsedData = JSON.parse(JSON.stringify(data.values))
+                console.log(parsedData)
+                var formattedData = []
+                // Reformat data into date and floats.
+                for (var currentInterval in parsedData) {
+                    formattedData.push(
+                        [
+                            +new Date(parsedData[currentInterval].datetime),
+                            parseFloat(parsedData[currentInterval].open),
+                            parseFloat(parsedData[currentInterval].high),
+                            parseFloat(parsedData[currentInterval].low),
+                            parseFloat(parsedData[currentInterval].close)
+                        ]
+                    )
+                }
+                console.log(formattedData)
+                const newData = [{data: formattedData}]
+                // Update data for graph.
+                that.setState({series: newData});
+            }
+        )
+    }
+
+
+    componentDidMount() {
+        this.fetchStock();
+    }
+
+    render(){
+        return (
+            <Chart
+              options={this.state.options}
+              series={this.state.series}
+              type="candlestick"
+              height={this.state.height}
+              width={this.state.width}
+            />
+        );
+    }
+}
+export default CandleStickChart;
--- js/market_manager/src/Pages/Home.js
+++ js/market_manager/src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import CandleStickChart from '../Components/CandleStickChart';
 
 class Home extends Component {
     render() {
         return (
 // Hella home page headers to check how footer worked (lol)
             <div>
+                <CandleStickChart ticker='AAPL' type='time_series' interval='1day' outputCount={100} height={450} width='50%'/>
                 <h1>Home Page</h1>
                 <h1>Home Page</h1>
                 <h1>Home Page</h1>

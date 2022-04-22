0e736d43d345d3c5ec2e77778c89c0361da9455a
rx789 <raz@uktechmail.com>
2022-04-22 23:03:57 +0100

    Tweaked footer

--- market_manager/react-frontend/src/components/theFooter.js
+++ market_manager/react-frontend/src/components/theFooter.js
@@ -9,8 +9,8 @@ function Footer() {
                 <div className="container">
                     <div className="row">
                         <div className="footer col d-flex justify-content-around align-items-center">
-                            <h7>Created By James, Jon, Raz, Owen, Matty, and Simon </h7>
-                            <h7>&copy;{new Date().getFullYear()} COMP208 </h7>
+                            <h6>Created By James, Jon, Raz, Owen, Matty, and Simon </h6>
+                            <h6>&copy;{new Date().getFullYear()} COMP208 </h6>
                             
                         {/* <p className="text-xs-center">
                             
